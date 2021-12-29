40fe729cc68e27d344513791b6bf2f9e3b5df295
Ramakrishnan <raminzenith@yahoo.co.in>
2021-12-29 22:12:32 +0530

    VM Build

--- main.tf
+++ main.tf
@@ -34,8 +34,103 @@ provider "azuread" {
     alias           =   "ad"
 }
 
-resource "azurerm_resource_group" "rg" {
-    name                  =   "${var.prefix}-rg"
-    location              =   var.location
+
+data "azurerm_resource_group" "rg" {
+  name = "TestGroup"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                  =   "${var.prefix}-vnet"
+    resource_group_name   =   azurerm_resource_group.rg.name
+    location              =   azurerm_resource_group.rg.location
+    address_space         =   [var.vnet_address_range]
     tags                  =   var.tags
 }
+
+resource "azurerm_subnet" "sn" {
+    for_each              =   var.subnets
+    name                  =   each.key
+    resource_group_name   =   azurerm_resource_group.rg.name
+    virtual_network_name  =   azurerm_virtual_network.vnet.name
+    address_prefixes      =   [each.value]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+    name                        =       "${var.prefix}-web-nsg"
+    resource_group_name         =       azurerm_resource_group.rg.name
+    location                    =       azurerm_resource_group.rg.location
+    tags                        =       var.tags
+
+    security_rule {
+    name                        =       "Allow_RDP"
+    priority                    =       1000
+    direction                   =       "Inbound"
+    access                      =       "Allow"
+    protocol                    =       "Tcp"
+    source_port_range           =       "*"
+    destination_port_range      =       3389
+    source_address_prefix       =       "YOUR_IP" 
+    destination_address_prefix  =       "*"
+    
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "server-subnet-nsg" {
+    subnet_id                    =       azurerm_subnet.sn["server-subnet"].id
+    network_security_group_id    =       azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "member-subnet-nsg" {
+    subnet_id                    =       azurerm_subnet.sn["member-subnet"].id
+    network_security_group_id    =       azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "pip" {
+    name                            =     "${var.prefix}-public-ip"
+    resource_group_name             =     azurerm_resource_group.rg.name
+    location                        =     azurerm_resource_group.rg.location
+    allocation_method               =     var.allocation_method[0]
+    tags                            =     var.tags
+}
+
+resource "azurerm_network_interface" "nic" {
+    name                              =   "${var.prefix}-nic"
+    resource_group_name               =   azurerm_resource_group.rg.name
+    location                          =   azurerm_resource_group.rg.location
+    tags                              =   var.tags
+    ip_configuration                  {
+        name                          =  "${var.prefix}-nic-ipconfig"
+        subnet_id                     =   azurerm_subnet.sn["server-subnet"].id
+        public_ip_address_id          =   azurerm_public_ip.pip.id
+        private_ip_address_allocation =   var.allocation_method[1]
+    }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+    name                              =   "${var.prefix}-vm"
+    resource_group_name               =   azurerm_resource_group.rg.name
+    location                          =   azurerm_resource_group.rg.location
+    network_interface_ids             =   [azurerm_network_interface.nic.id]
+    size                              =   var.virtual_machine_size
+    computer_name                     =   var.computer_name
+    admin_username                    =   var.admin_username
+    admin_password                    =   var.admin_password
+
+    os_disk  {
+        name                          =   "${var.prefix}-os-disk"
+        caching                       =   var.os_disk_caching
+        storage_account_type          =   var.os_disk_storage_account_type
+        disk_size_gb                  =   var.os_disk_size_gb
+    }
+
+    source_image_reference {
+        publisher                     =   var.publisher
+        offer                         =   var.offer
+        sku                           =   var.sku
+        version                       =   var.vm_image_version
+    }
+
+    tags                              =   var.tags
+
+}
+
--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "tags" {
     description =   "Resouce tags"
     type        =   map(string)
     default     =   {
-        "author"        =   "Vamsi"
+        "author"        =   "Ram"
         "deployed_with" =   "Terraform"
     }
 }
@@ -41,4 +41,77 @@ variable "location" {
     description =   "Location of the resource group"
     type        =   string
     default     =   "East US"
+}
+
+variable "vnet_address_range" {
+    description =   "IP Range of the virtual network"
+    type        =   string
+    default     =   "10.0.0.0/16"
+}
+
+variable "subnets" {
+    description =   "subnets attributes"
+    type        =   map(string)
+    default     =   {
+    "server-subnet"    =   "10.0.1.0/24"
+    "member-subnet"    =   "10.0.2.0/24"
+    }
+}
+
+variable "allocation_method" {
+    description =   "Allocation method for Public IP Address and NIC Private ip address"
+    type        =   list(string)
+    default     =   ["Static", "Dynamic"]
+}
+
+variable "virtual_machine_size" {
+    description =   "Size of the VM"
+    type        =   string
+    default     =   "Standard_D2s_v3"
+}
+
+variable "computer_name" {
+    description =   "Computer name"
+    type        =   string
+    default     =   "Win10vm"
+}
+
+variable "admin_username" {
+    description =   "Username to login to the VM"
+    type        =   string
+    default     =   "winserveradmin"
+}
+
+variable "admin_password" {
+    description =   "Password to login to the VM"
+    type        =   string
+    default     =   "P@$$w0rD2020*"
+}
+
+variable "os_disk_caching" {
+    default     =       "ReadWrite"
+}
+
+variable "os_disk_storage_account_type" {
+    default     =       "StandardSSD_LRS"
+}
+
+variable "os_disk_size_gb" {
+    default     =       128
+}
+
+variable "publisher" {
+    default         =       "MicrosoftWindowsServer"
+}
+
+variable "offer" {
+    default         =       "WindowsServer"
+}
+
+variable "sku" {
+    default         =       "2019-Datacenter"
+}
+
+variable "vm_image_version" {
+    default         =       "latest"
 }
