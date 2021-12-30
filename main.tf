95a47afdbb5874d4a080d9d243369b6757a65192
Ramakrishnan <raminzenith@yahoo.co.in>
2021-12-30 14:37:30 +0530

    main

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                    =       "Tcp"
     source_port_range           =       "*"
     destination_port_range      =       3389
-    source_address_prefix       =       "YOUR_IP" 
+    source_address_prefix       =       "192.168.1.36" 
     destination_address_prefix  =       "*"
     
     }
